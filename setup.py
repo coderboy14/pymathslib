7a722d99c1fc72747ea15104d1de32c51746e868
Ethan Manzi <coderboy14@amazedkeys.com>
2020-11-05 15:10:10 -0500

    Inital commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mathslib",
+    packages=find_packages(),
+    version="1.0.0",
+    description="Math library",
+    author="Ethan Manzi",
+    license="MIT",
+    install_requires=[],
+    setup_requires=[""],
+    tests_require=[""],
+    # test_suite="tests",
+)
